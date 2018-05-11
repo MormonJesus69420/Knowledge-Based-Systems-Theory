07ebc99b7f8e5f9e8dc0e7aa08d4aee8b642b673
MormonJesus69420 <dsa014@post.uit.no>
2018-05-11 18:47:08 +0200

    Box finished Roulette implemented, work on fitness

--- ass5/box.py
+++ ass5/box.py
@@ -15,23 +15,20 @@ from bitstring import BitArray
 
 
 class Box:
-    length = 5
+    def __init__(self, chromosome: BitArray, length=8):
+        self._length = length
+        self.x_chromosome = chromosome[0:length]
+        self.y_chromosome = chromosome[length:length * 2]
 
-    def __init__(self, chromosome: BitArray):
-        self.x_chromosome = chromosome[0:self.length]
-        self.y_chromosome = chromosome[self.length:self.length * 2]
+    def crossover(self, box: 'Box') -> List['Box']:
 
-    @staticmethod
-    def crossover(box1: 'Box', box2: 'Box') -> List['Box']:
-        length = Box.length
+        cutoff = randint(1, self.length - 1)
+        x_1 = self.x_chromosome[0:cutoff] + box.x_chromosome[cutoff:self.length]
+        x_2 = box.x_chromosome[0:cutoff] + self.x_chromosome[cutoff:self.length]
 
-        cutoff = randint(1, length - 1)
-        x_1 = box1.x_chromosome[0:cutoff] + box2.x_chromosome[cutoff:length]
-        x_2 = box2.x_chromosome[0:cutoff] + box1.x_chromosome[cutoff:length]
-
-        cutoff = randint(1, length - 1)
-        y_1 = box1.y_chromosome[0:cutoff] + box2.y_chromosome[cutoff:length]
-        y_2 = box2.y_chromosome[0:cutoff] + box1.y_chromosome[cutoff:length]
+        cutoff = randint(1, self.length - 1)
+        y_1 = self.y_chromosome[0:cutoff] + box.y_chromosome[cutoff:self.length]
+        y_2 = box.y_chromosome[0:cutoff] + self.y_chromosome[cutoff:self.length]
 
         return [Box(BitArray(x_1 + y_1)), Box(BitArray(x_2 + y_2))]
 
@@ -39,23 +36,22 @@ class Box:
         self.x_chromosome.invert(randint(0, self.length - 1))
         self.y_chromosome.invert(randint(0, self.length - 1))
 
+    def _set_fitness(self, fitness: int):
+        self._fitness = fitness
 
-b1 = Box(BitArray('0b1111111111'))
-b2 = Box(BitArray('0b0000000000'))
+    def _get_fitness(self):
+        return self._fitness
 
-print(BitArray('0b11111').int)
+    def _get_length(self):
+        return self._length
 
-new = Box.crossover(b1, b2)
+    def _get_x(self):
+        return self.x_chromosome.uint / 10.0
 
-b1 = new[0]
-b2 = new[1]
+    def _get_y(self):
+        return self.y_chromosome.uint / 10.0
 
-print(b1.x_chromosome.bin)
-print(b1.y_chromosome.bin)
-print(b2.x_chromosome.bin)
-print(b2.y_chromosome.bin)
-
-b1.mutate()
-
-print(b1.x_chromosome.bin)
-print(b1.y_chromosome.bin)
+    x = property(_get_x)
+    y = property(_get_y)
+    length = property(_get_length)
+    fitness = property(_get_fitness, _set_fitness)
--- ass5/geneticalgorithm.py
+++ ass5/geneticalgorithm.py
@@ -7,9 +7,93 @@ Version:  69.420
 Author:   MormonJesus69420
 Date:     Yes, please
 """
+from random import random
+
+from typing import List
+
 from bitstring import BitArray
 
+from box import Box
+
+from math import ceil
 
 class GeneticAlgorithm:
 
+    def calculate_fitness(self, population: List[Box]) -> None:
+        for box in population:
+            self.correct_quadrant_check(box)
+            self.least_effort_check(box)
 
+    def correct_quadrant_check(self, box: Box) -> None:
+        pass
+
+    def least_effort_check(self, box: Box) -> None:
+        pass
+
+    def roulette(self, population: List[Box]) -> List[Box]:
+        # Get total fitness
+        total = sum(box.fitness for box in population)
+
+        # Normalize fitness of each box to values 0-1
+        [self._normalize_fitness(box, total) for box in population]
+
+        # Create list of accumulated fitness scores
+        acc = list()
+        temp = 0.0
+        for box in population:
+            temp += box.fitness
+            acc.append(temp)
+
+        # Breed new population
+        new_pop = list()
+        for _ in range(0, len(population)):
+            daddy = population[self._get_index(acc, random())]
+            mommy = population[self._get_index(acc, random())]
+            # Do not accept breeding with itself
+            while daddy == mommy:
+                mommy = population[self._get_index(acc, random())]
+            babies = mommy.crossover(daddy)
+            new_pop.append(babies[0])
+            new_pop.append(babies[1])
+
+        return new_pop
+
+    def _normalize_fitness(self, box: Box, total_fitness: int) -> None:
+        box.fitness /= total_fitness
+
+    def _get_index(self, acc: List[float], percentage: float) -> int:
+        for i in range(0, len(acc)):
+            if percentage < acc[i]:
+                return i
+
+# Random population values guaranteed to be random
+# pop = [Box(BitArray('0b1100011101000001')),
+#        Box(BitArray('0b1001100101100011')),
+#        Box(BitArray('0b1010101010100111')),
+#        Box(BitArray('0b0111000001011111')),
+#        Box(BitArray('0b1110111010000101')),
+#        Box(BitArray('0b1110000011011011')),
+#        Box(BitArray('0b1110001001101010')),
+#        Box(BitArray('0b0100010100100011')),
+#        Box(BitArray('0b1111010000101101')),
+#        Box(BitArray('0b0110011101101011')),
+#        Box(BitArray('0b0111101000111011')),
+#        Box(BitArray('0b1100001010101000')),
+#        ]
+
+r = GeneticAlgorithm().roulette
+pop = [Box(BitArray('0b1111111111111111')),
+       Box(BitArray('0b0000000000000000')),
+       Box(BitArray('0b0101010101010101')),
+       Box(BitArray('0b1111111100000000')),
+       ]
+
+pop[0].fitness = 50
+pop[1].fitness = 30
+pop[2].fitness = 15
+pop[3].fitness = 5
+
+meh = r(pop)
+
+for box in meh:
+    print(box.x_chromosome.bin)
