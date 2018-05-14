8b05c4cd94664f3202c42ae50570b9ae05ed965c
Mormon Jesus <Egeikkjenorsk@gmail.com>
2018-05-14 16:54:08 +0200

    Working on assignment 7

---
+++ ass7/perceptron.py
@@ -0,0 +1,15 @@
+#!/home/mormonjz/Anaconda3/bin/python python3
+# -*- coding: utf-8 -*-
+"""
+Perceptron class represents a simplified neuron that categorizes inputs based on x and y values.
+
+Version:  69.420
+Author:   MormonJesus69420
+Date:     Yes, please
+"""
+
+
+class Perceptron:
+    def __init__(self):
+        self.dick = "Dick"
+
